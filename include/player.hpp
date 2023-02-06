8921915a126cbc5f38480fa7c9c2b8fa6fb96ef1
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-02-06 23:01:27 +0000

    Update player.hpp

--- include/player.hpp
+++ include/player.hpp
@@ -3,48 +3,58 @@
 #ifndef PLAYER
 #define PLAYER
 
-#define SMALL_WEIGHTS typedef enum 
-#define MEDIUM_WEIGHTS typedef enum
-#define LARGE_WEIGHTS typedef enum
+#include "common.h"
 
-SMALL_WEIGHTS
+#ifndef WEIGHT_CLASSES
+#define WEIGHT_CLASSES
+
+typedef struct SMALL_WEIGHTS;
+typedef struct MEDIUM_WEIGHTS;
+typedef struct LARGE_WEIGHTS;
+
+typedef enum SMALL : U8
 {
-	    MARIO = 0x06,
-		LUIGI = 0x0C,
-		PEACH = 0x01,
-		DAISY = 0x04,
-		TOAD = 0x08,
-		TOADETTE = 0x0D,
-		KOOPA = 0x0E,
-		DRY_BONES = 0x05
+	MARIO = 0x6,
+	LUIGI = 0xC,
+	PEACH = 0x10,
+	DAISY = 0x4,
+	TOAD = 0x8,
+	TOADETTE = 0xD,
+	KOOPA = 0xE,
+	DRY_BONES = 0x5
+
 };
 
-MEDIUM_WEIGHTS
-{  
-	MARIO = 0x00,
-	LUIGI = 0x07,
+typedef enum MEDIUM : U8
+{
+	MARIO = 0x0,
+	LUIGI = 0x7,
 	PEACH = 0x10,
-	DAISY = 0x0F,
-	YOSHI = 0x0A,
+	DAISY = 0xF,
+	YOSHI = 0xA,
 	BIRDO = 0x11,
 	DIDDY = 0x12,
 	BOWSER_JR = 0x14
+
 };
 
-LARGE_WEIGHTS
+typedef enum LARGE : U8
 {
-	WARIO = 0x0B,
-	WALUIGI = 0x02,
-	DONKEY = 0x09,
-	BOWSER = 0x03,
+	WARIO = 0xB,
+	WALUIGI = 0x2,
+	DONKEY = 0x9,
+	BOWSER = 0x3,
 	KING_BOO = 0x13,
 	ROSALINA = 0x17,
 	FUNKY = 0x16,
-	DRY_BOWSER = 0x15  
+	DRY_BOWSER = 0x15
+
 };
 
-SMALL_WEIGHTS SMALL;
-MEDIUM_WEIGHTS MEDIUM;
-LARGE_WEIGHTS LARGE;
+SMALL_WEIGHTS* SMALL();
+MEDIUM_WEIGHTS* MEDIUM();
+LARGE_WEIGHTS* LARGE();
+
+#endif
 
 #endif 
