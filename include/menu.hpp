698a14e3e5316b3207275a882e51a084b58e4c54
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-02-06 23:42:33 +0000

    Add files via upload

---
+++ include/menu.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#ifndef MENU
+#define MENU
+
+#include "common.h"
+#include "dvd.h"
+#include "race.h"
+
+inline void DVD_FUNCTIONS::SET_ARCHIVE_HEAP()
+{
+
+};
+
+
+inline void DVD_FUNCTIONS::SET_FILE_HEAP()
+{
+
+}
+
+#endif
